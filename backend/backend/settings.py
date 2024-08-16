ed52fca5818e6bc6f09f21571eaf40c8c887f9c1
jamestha3d <jamesikediashi@gmail.com>
2024-08-16 01:48:27 -0400

    fixed dot_env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,6 +15,8 @@ from datetime import timedelta
 # Add these at the top of your settings.py
 from os import getenv
 from dotenv import load_dotenv
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
